21afa293e5e6d6d40c682b0bdf82f95ce18f2bbe
Anteczko <s21@onet.pl>
2019-04-05 23:24:27 +0200

    2

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install xinit
+sudo apt install x11-xserver-utils
+sudo apt install thunar feh
+
+sudo apt install build-essential git cmake cmake-data pkg-config libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev -y
