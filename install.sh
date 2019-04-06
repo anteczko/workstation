4970ad8bf0e6a4a0b6464b31922da33a18f0cc7d
anteczko <35063081+anteczko@users.noreply.github.com>
2019-04-06 17:54:11 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,20 @@ sudo apt install x11-xserver-utils
 sudo apt-get install software-properties-common
 sudo apt-get update
 sudo apt-get install libxcb-xrm-dev
+sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake
+
+cd /tmp
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+git checkout gaps && git pull
+autoreconf --force --install
+rm -rf build
+mkdir build
+cd build
+../configure --prefix=/usr --sysconfdir=/etc
+make
+sudo make install
+
 #sudo apt install thunar feh
 
 #sudo apt install build-essential git cmake cmake-data pkg-config libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev -y
