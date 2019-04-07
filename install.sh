a7f5aa0d13a2e818a3065890ebc7d35369acd9cc
anteczko <s21@onet.pl>
2019-04-07 14:04:39 +0200

    6

--- install.sh
+++ install.sh
@@ -4,10 +4,12 @@ sudo apt-get install xinit
 sudo apt install x11-xserver-utils
 sudo apt-get install software-properties-common
 sudo apt-get update
+
+#install i3 dependences
 sudo apt-get install libxcb-xrm-dev
 sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake
-
-
+'''
+#instal i3-gaps
 git clone https://www.github.com/Airblader/i3 i3-gaps
 cd i3-gaps
 git checkout gaps && git pull
@@ -19,6 +21,17 @@ cd build
 make
 sudo make install
 
+'''
 #sudo apt install thunar feh
+#instal polybar dependeces
+sudo apt install build-essential git cmake cmake-data pkg-config libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev -y
+cd ~
+git clone --recursive https://github.com/jaagr/polybar
+cd polybar
+mkdir build
+cd build
+cmake ..
+make -j$(nproc)
+sudo make install
 
-#sudo apt install build-essential git cmake cmake-data pkg-config libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev -y
+
